95c4bb9d59cc53bd32576d67937e2c22cf15da08
TomYou2018 <35715313+TomYou2018@users.noreply.github.com>
2018-05-29 12:54:30 +0800

    a commit for succesful hello

---
+++ demo-spring-boot-1few/src/main/java/com/onefew/springboot/DemoSpringBoot1fewApplication.java
@@ -0,0 +1,12 @@
+package com.onefew.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBoot1fewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBoot1fewApplication.class, args);
+    }
+}
