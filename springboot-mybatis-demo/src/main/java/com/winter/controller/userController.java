444b66e1e1ab43f82ec59a692f0147fc35592fcf
TomYou2018 <35715313+TomYou2018@users.noreply.github.com>
2018-05-29 16:47:41 +0800

    2

--- springboot-mybatis-demo/src/main/java/com/winter/controller/userController.java
+++ springboot-mybatis-demo/src/main/java/com/winter/controller/userController.java
@@ -1,5 +1,4 @@
 package com.winter.controller;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
@@ -7,24 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.winter.service.user.UserService;
 import com.winter.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
 
 @Controller
-@EnableAutoConfiguration
 @RequestMapping("/user")
-
 public class UserController {
-   //@Autowired
-    private UserService userService;
-
-    @ResponseBody
-    @RequestMapping(value = "/add")
-    public int addUser(User user){
-        return userService.addUser(user);
-    }
 
     @ResponseBody
     @RequestMapping(value = "/dsp")
--- springboot-mybatis-demo/src/main/java/com/winter/springbootmybatisdemo/SpringbootMybatisDemoApplication.java
+++ springboot-mybatis-demo/src/main/java/com/winter/springbootmybatisdemo/SpringbootMybatisDemoApplication.java
@@ -1,4 +1,5 @@
 package com.winter.springbootmybatisdemo;
+import org.mybatis.spring.annotation.MapperScan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +7,8 @@ import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 
-//@MapperScan("com.winter.mapper")//将项目中对应的mapper类的路径加进来就可以了
-//@Service(value = 'userMapper')
-//public interface UserMapper
+@MapperScan("com.winter.mapper")//将项目中对应的mapper类的路径加进来就可以了
+
 
 public class SpringbootMybatisDemoApplication {
 
